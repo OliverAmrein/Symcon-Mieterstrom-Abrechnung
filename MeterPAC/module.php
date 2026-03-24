0074d28e4d5f38e86c5909b1013f2b82582fbca4
OliverAmrein <info@amrein-projekt.ch>
2026-03-24 19:24:55 +0100

    aaa

--- MeterPAC/module.php
+++ MeterPAC/module.php
@@ -33,7 +33,10 @@ class MeterPAC extends IPSModule
         IPS_SetProperty($this->InstanceID, 'Anlagenkennzeichen', $json['DEVICE_INFO']['AKZ']);
         IPS_SetProperty($this->InstanceID,'Ortskennzeichen', $json['DEVICE_INFO']['OKZ']);
         
-        PAC_ApplyChanges();
+        $this->UpdateFormField('Anlagenkennzeichen', 'value', $json['DEVICE_INFO']['AKZ']);
+        $this->UpdateFormField('Ortskennzeichen', 'value', $json['DEVICE_INFO']['OKZ']);
+
+        PAC_ApplyChanges($id);
 
         //SetValue($this->GetIDForIdent("Anlagenkennzeichen"), $json['DEVICE_INFO']['AKZ']);
         //SetValue($this->GetIDForIdent("Ortskennzeichen"), $json['DEVICE_INFO']['OKZ']);
