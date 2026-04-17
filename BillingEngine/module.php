6509ef213fe3e98a1f33dff826e0e059589e2b55
OliverAmrein <info@amrein-projekt.ch>
2026-04-17 19:14:11 +0200

    asd

--- BillingEngine/module.php
+++ BillingEngine/module.php
@@ -58,7 +58,7 @@ class BillingEngine extends IPSModule
 
         //echo $Mietername.'_'.$Startdatum.'_'.$Enddatum.PHP_EOL;
 
-        $vormonat = date("Y-m-d", strtotime("-1 month", strtotime($Startdatum)));
+        $vormonat = date("Y-m-d", strtotime("-1 month", $Startdatum));
         $erstertag = strtotime(date('Y-m-01', $vormonat));
         $letztertag = strtotime(date('Y-m-t', $vormonat));
 
@@ -419,7 +419,7 @@ class BillingEngine extends IPSModule
 		$BetragInclMwstGerundet = $gerundet;
 
 
-        $vormonat = date("Y-m-d", strtotime("-1 month", strtotime($Startdatum)));
+        $vormonat = date("Y-m-d", strtotime("-1 month", $Startdatum));
 		$ersterTag = date('01.m.Y', strtotime($vormonat));
 		$letzterTag = date('t.m.Y', strtotime($vormonat));
 		
