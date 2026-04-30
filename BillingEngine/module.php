ad5d8a19cc79aa100d125fc1299e6edfd0b81d61
OliverAmrein <info@amrein-projekt.ch>
2026-04-30 16:02:09 +0200

    asd

--- BillingEngine/module.php
+++ BillingEngine/module.php
@@ -613,7 +613,7 @@ class BillingEngine extends IPSModule
     {
         $data = $this->FetchData($Startdatum, $Enddatum, $MieterID);
         if ($data == []) {
-            echo 'xxxxxxxxxxxxx empty data xxxxxxxxxxxxxx';
+            echo 'xxxxxxxxxxxxx empty data 1 xxxxxxxxxxxxxx '.$Startdatum.'  ".$Enddatum.'  '.$MieterID';
             return;
         }
         global $Bezug; // berechne hier für Seite 1
@@ -650,7 +650,7 @@ class BillingEngine extends IPSModule
     {
         $data = $this->FetchData($Startdatum, $Enddatum, $MieterID);
         if ($data == []) {
-            echo 'xxxxxxxxxxxxx empty data xxxxxxxxxxxxxx';
+            echo 'xxxxxxxxxxxxx empty data 2 xxxxxxxxxxxxxx '.$Startdatum.'  ".$Enddatum.'  '.$MieterID';
             return;
         }
         global $Bezug; // berechne hier für Seite 1
